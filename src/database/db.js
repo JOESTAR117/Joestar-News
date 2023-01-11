db815485696e4966baa74f3b9b80676c7c29fdbb
Jackson Rios <juniorrios1231@gmail.com>
2023-01-11 16:34:04 -0300

    connect MongoDB

---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  console.log("wait connecting to the database");
+
+  mongoose
+    .connect(
+      "mongodb+srv://jackson:031019maepai@cluster0.d0nfwjz.mongodb.net/?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    .then(() => console.log("MongoDB atlas Connectd"))
+    .catch((erro) => console.error(erro.message));
+};
+module.exports = connectDatabase;
