ada058893297ce020b7331c9ec03ac5c9a0f8da1
Jackson Rios <juniorrios1231@gmail.com>
2023-01-12 14:21:22 -0300

    doenv add

--- src/database/db.js
+++ src/database/db.js
@@ -4,8 +4,7 @@ const connectDatabase = () => {
   console.log("wait connecting to the database");
 
   mongoose
-    .connect(
-      "mongodb+srv://jackson:031019maepai@cluster0.d0nfwjz.mongodb.net/?retryWrites=true&w=majority",
+    .connect( process.env.MONGODB_URI,
       { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log("MongoDB atlas Connectd"))
