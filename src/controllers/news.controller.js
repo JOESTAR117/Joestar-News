6b01a35d023e2c40d4fb3fc53fee89393f120c40
Jackson Rios <juniorrios1231@gmail.com>
2023-01-13 14:57:32 -0300

    searchByTitle

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -4,6 +4,7 @@ import {
   countNews,
   topNewsService,
   findByIdService,
+  searchByTitleService,
 } from "../services/news.service.js";
 import { ObjectId } from "mongoose";
 
@@ -128,6 +129,35 @@ export const findById = async (req, res) => {
         userName: news.user.username,
         userAvatar: news.user.avatar,
       },
+    });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
+
+export const searchByTitle = async (req, res) => {
+  try {
+    const { title } = req.query;
+
+    const news = await searchByTitleService(title);
+
+    if (news.length === 0) {
+      return res
+        .status(400)
+        .send({ message: "There are no news with this title" });
+    }
+    return res.send({
+      results: news.map((item) => ({
+        id: item._id,
+        title: item.title,
+        text: item.text,
+        banner: item.banner,
+        likes: item.likes,
+        comments: item.comments,
+        name: item.user.name,
+        userName: item.user.username,
+        userAvatar: item.user.avatar,
+      })),
     });
   } catch (err) {
     res.status(500).send(err.message);
