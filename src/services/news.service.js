068226359da7da6c2dec771ce18accb02169214a
Jackson Rios <juniorrios1231@gmail.com>
2023-01-13 13:27:03 -0300

    topNews

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -1,16 +1,12 @@
 import News from "../models/News.js";
 
-const createService = (body) => News.create(body)
+const createService = (body) => News.create(body);
 
-const findAllService = (offset,limit) => News.find().sort({_id: -1}).skip(offset).limit(limit).populate("user")
+const findAllService = (offset, limit) =>
+  News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
 
 const countNews = () => News.countDocuments();
 
+const topNewsService = () => News.findOne().sort({ _id: -1 }).populate("user");
 
-
-
-export {
-    createService,
-    findAllService,
-    countNews
-}
+export { createService, findAllService, countNews, topNewsService };
